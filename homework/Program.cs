8f969dc733e791e1819f2c11969af7e011504984
Юрий <nsitc@mail.ru>
2023-12-30 05:44:50 +0400

    Формируем новый массив, выводим результат

--- homework/Program.cs
+++ homework/Program.cs
@@ -1,3 +1,12 @@
 ﻿Console.WriteLine("Введите элементы массива строк через запятую:");
 string[] inputArray = Console.ReadLine().Split(',');
 
+string[] resultArray = FilterStrings(inputArray);
+
+Console.WriteLine("Новый массив строк c длиной <= 3 символов:");
+foreach (var str in resultArray)
+{
+    Console.WriteLine(str);
+}
+   
+
