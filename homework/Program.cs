676515beae06ecb2c962893df732c11c6f4fc2ea
Юрий <nsitc@mail.ru>
2023-12-30 05:55:16 +0400

    Вывод нового массива с длинной строк не более 3-х символов

--- homework/Program.cs
+++ homework/Program.cs
@@ -1,12 +1,49 @@
-﻿Console.WriteLine("Введите элементы массива строк через запятую:");
-string[] inputArray = Console.ReadLine().Split(',');
+﻿using System;
 
-string[] resultArray = FilterStrings(inputArray);
-
-Console.WriteLine("Новый массив строк c длиной <= 3 символов:");
-foreach (var str in resultArray)
+class Program
 {
-    Console.WriteLine(str);
-}
-   
+    static void Main()
+    {
+        Console.WriteLine("Введите элементы массива строк через запятую:");
+        string[] inputArray = Console.ReadLine().Split(',');
 
+        // Формируем новый массив из строк, длина которых <= 3 символов
+        string[] resultArray = FilterStrings(inputArray);
+
+        // Выводим результат
+        Console.WriteLine("Новый массив строк c длиной <= 3 символов:");
+        foreach (var str in resultArray)
+        {
+            Console.WriteLine(str);
+        }
+    }
+
+    // Метод для фильтрации строк в массиве
+    static string[] FilterStrings(string[] inputArray)
+    {
+        int count = 0; // Счетчик строк, удовлетворяющих условию
+        for (int i = 0; i < inputArray.Length; i++)
+        {
+            if (inputArray[i].Length <= 3)
+            {
+                count++;
+            }
+        }
+
+        // Создаем новый массив нужного размера
+        string[] resultArray = new string[count];
+        int index = 0;
+
+        // Заполняем новый массив строками, удовлетворяющими условию
+        for (int i = 0; i < inputArray.Length; i++)
+        {
+            if (inputArray[i].Length <= 3)
+            {
+                resultArray[index] = inputArray[i];
+                index++;
+            }
+        }
+
+        return resultArray;
+    }
+}
